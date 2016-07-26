b3da80a186d8abfc8b8c5448aab685e170f652a0
quake0day <quake0day@gmail.com>
2016-07-26 16:26:38 -0400

    add reset func

---
+++ enableAccount.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from Account import Account
+
+i = 0 
+while 1:
+    account = Account.query.filter(Account.isUsed == 2).first()
+    if account == None:
+        break
+    account.reset()
+    account.save()
+    username, password, auth = account.getInfo()
+    i += 1
+    print "Reset Account: " + username +" " + password + " " + auth
+
+print "Total Reset:" + str(i) + " accounts"
+
+
